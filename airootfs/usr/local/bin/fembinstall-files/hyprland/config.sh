6959bbd1783f04e7973ad28a1c0065fbb6a5e5df
Fembarch <clubconnection0@gmail.com>
2026-06-04 08:02:23 -0300

    Update config.sh

--- airootfs/usr/local/bin/fembinstall-files/hyprland/config.sh
+++ airootfs/usr/local/bin/fembinstall-files/hyprland/config.sh
@@ -1,10 +1,1 @@
-#!/usr/bin/env bash
-
-# Make sure the config files exist inside of the .config/ directory.
-# If .config/hypr doesn't exist then create it
-if [[ ! -d "$HOME"/.config/hypr/ ]]; then
-    mkdir -p "$HOME"/.config/hypr
-elif [[ ! -f "$HOME"/.config/hypr/hyprland.conf ]]; then
-    cp /usr/share/hypr/hyprland.conf "$HOME"/.config/hypr/hyprland.conf
-fi
 
